efcc227f64da46c30dd7d06993228915a68dacc7
汐若瞳梵君 <99623463+lixu0073@users.noreply.github.com>
2025-07-02 10:47:42 +0800

    Add files via upload

---
+++ SafeAreaFitter.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeAreaFitter : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private Rect safeAreaRect;
+    private Rect lastSafeAreaRect;
+    private ScreenOrientation lastScreenOrientation = ScreenOrientation.AutoRotation;
+
+    IEnumerator Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        // 等待当前帧的所有渲染和UI计算结束后再执行
+        yield return new WaitForEndOfFrame();
+        ApplySafeArea();
+    }
+
+    void Update()
+    {
+        if (Screen.safeArea != lastSafeAreaRect || Screen.orientation != lastScreenOrientation)
+        {
+            ApplySafeArea();
+        }
+    }
+
+    private void ApplySafeArea()
+    {
+        safeAreaRect = Screen.safeArea;
+        lastScreenOrientation = Screen.orientation;
+
+        Vector2 anchorMin = safeAreaRect.position;
+        Vector2 anchorMax = safeAreaRect.position + safeAreaRect.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        rectTransform.anchorMin = anchorMin;
+        rectTransform.anchorMax = anchorMax;
+
+        rectTransform.offsetMin = Vector2.zero;
+        rectTransform.offsetMax = Vector2.zero;
+    }
+}
